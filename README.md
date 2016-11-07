96e24afe3c31d3151930503664d5bca0ae0c14d2
Mark Baggett <lo127001@gmail.com>
2016-11-07 16:35:48 -0500

    Added 'seconds' formatting. Well known SID lookup. Updated template to calculate connected times.  More error handling.

--- README.md
+++ README.md
@@ -1,25 +1,24 @@
 # srum-dump
 
-This program will create an excel spreadsheet containing forensics artifacts contained the SRUM (System Resource Utilization Manager) data base.
+This program will create an excel spreadsheet containing forensics artifacts contained the SRUM (System Resource Utilization Manager) database.
 
-The program can be run with no input and it will prompt you for each of the needed arguments.   The program requires two inputs.  The first is an SRUM database.   The SRUM database is usually \Windows\system32\sru\SRUDB.dat.    The second is a XLSX template file that contains the names of the tables and fields that you want to extract from the SRUM database.   SRUM_TEMPLATE.xlsx is distributed with the program and it includes the fields that you will most likely be interested in.  If you want to add additinal XLS calculations or remove fields form the SRUM database you can make a copy of SRUM_TEMPLATE.xlsx and change that template file as needed.   The program will create a new XLSX file containing the fields specified inside the template.
+The program can be run with no input and it will prompt you for each of the needed arguments. The program requires two inputs. The first is an SRUM database. The SRUM database is usually \Windows\system32\sru\SRUDB.dat. The second is a XLSX template file that contains the names of the tables and fields that you want to extract from the SRUM database. SRUM_TEMPLATE.xlsx is distributed with the program and it includes the fields that you will most likely be interested in.  If you want to add additinal XLS calculations or remove fields form the SRUM database you can make a copy of SRUM_TEMPLATE.xlsx and change that template file as needed. The program will create a new XLSX file containing the fields specified inside the template.
 
-The program will also optionally take a SOFTWARE registry hive.   The path for this files is typically \Windows\System32\config\SOFTWARE
+The program will also optionally take a SOFTWARE registry hive. The path for this files is typically \Windows\System32\config\SOFTWARE
 
 
-Here is the usage statement.
+Here is the usage statement:
  
-usage: srum_dump.py [-h] [--ESE_INFILE ESE_INFILE]
+usage: srum_dump.py [-h] [--SRUM_INFILE ESE_INFILE]
                     [--XLSX_OUTFILE XLSX_OUTFILE]
                     [--XLSX_TEMPLATE XLSX_TEMPLATE] [--REG_HIVE REGHIVE]
                     [--quiet]
 
-Given an SRUM database it will create an XLS spreadsheet with analysis of the
-data in the database.
+Given an SRUM database it will create an XLS spreadsheet with analysis of the data in the database.
 
 optional arguments:
   -h, --help            show this help message and exit
-  --ESE_INFILE ESE_INFILE, -i ESE_INFILE
+  --SRUM_INFILE ESE_INFILE, -i ESE_INFILE
                         Specify the ESE (.dat) file to analyze. Provide a
                         valid path to the file.
   --XLSX_OUTFILE XLSX_OUTFILE, -o XLSX_OUTFILE
@@ -50,11 +49,16 @@ ROW 3  = Format commands to change that tell SRUM_DUMP how to interpret the data
     md5 (calucale a hash of the data)
     sha1 (calucale a hash of the data)
     sha256 (calucale a hash of the data)
+    seconds (convert the data to Days Hours:Minutes:Seconds)
 ROW 4  = Human Readable column names for the associated fields.  The contents of this row including its formatting will be come the columm headers in your resulting output xls file.
 
 See the SRUM_TEMPLATE.XLSX for an example.  Check out ESE_ANALYST from http://github.com/MarkBaggett/ for more details.
 
+Example:
+Assuming SRUM_TEMPLATE.XLSX is in the current directory:
+srum-dump.exe -i SRUDB.dat -r SOFTWARE -o output_report.xls
 
+If you just run srum-dump.exe it will prompt you for the required values.
 
 
 
--- srum_dump.py
+++ srum_dump.py
@@ -1,5 +1,5 @@
 from impacket import ese
-from datetime import datetime,timedelta
+from datetime import datetime,timedelta,time
 import os
 import sys
 import struct
@@ -11,8 +11,16 @@ import argparse
 import warnings
 import hashlib
 
+
+def lookup_sid(sid):
+    #Returns a common sid https://support.microsoft.com/en-us/kb/243330
+    known_sids = {'S-1-5-32-545': ' Users', 'S-1-5-32-544': ' Administrators', 'S-1-5-32-547': ' Power Users', 'S-1-5-32-546': ' Guests', 'S-1-5-32-569': ' BUILTIN\\Cryptographic Operators', 'S-1-16-16384': ' System Mandatory Level ', 'S-1-5-32-551': ' Backup Operators', 'S-1-16-8192': ' Medium Mandatory Level ', 'S-1-5-80': ' NT Service ', 'S-1-5-32-548': ' Account Operators', 'S-1-5-32-561': ' BUILTIN\\Terminal Server License Servers', 'S-1-5-64-14': ' SChannel Authentication ', 'S-1-5-32-562': ' BUILTIN\\Distributed COM Users', 'S-1-5-64-21': ' Digest Authentication ', 'S-1-5-19': ' NT Authority', 'S-1-3-0': ' Creator Owner', 'S-1-5-80-0': ' All Services ', 'S-1-5-20': ' NT Authority', 'S-1-5-18': ' Local System', 'S-1-5-32-552': ' Replicators', 'S-1-5-32-579': ' BUILTIN\\Access Control Assistance Operators', 'S-1-16-4096': ' Low Mandatory Level ', 'S-1-16-12288': ' High Mandatory Level ', 'S-1-2-0': ' Local ', 'S-1-16-0': ' Untrusted Mandatory Level ', 'S-1-5-3': ' Batch', 'S-1-5-2': ' Network', 'S-1-5-1': ' Dialup', 'S-1-5-7': ' Anonymous', 'S-1-5-6': ' Service', 'S-1-5-4': ' Interactive', 'S-1-5-9': ' Enterprise Domain Controllers', 'S-1-5-8': ' Proxy', 'S-1-5-32-550': ' Print Operators', 'S-1-0-0': ' Nobody', 'S-1-5-32-559': ' BUILTIN\\Performance Log Users', 'S-1-5-32-578': ' BUILTIN\\Hyper-V Administrators', 'S-1-5-32-549': ' Server Operators', 'S-1-2-1': ' Console Logon ', 'S-1-3-1': ' Creator Group', 'S-1-5-32-575': ' BUILTIN\\RDS Remote Access Servers', 'S-1-3-3': ' Creator Group Server', 'S-1-3-2': ' Creator Owner Server', 'S-1-5-32-556': ' BUILTIN\\Network Configuration Operators', 'S-1-5-32-557': ' BUILTIN\\Incoming Forest Trust Builders', 'S-1-5-32-554': ' BUILTIN\\Pre-Windows 2000 Compatible Access', 'S-1-5-32-573': ' BUILTIN\\Event Log Readers ', 'S-1-5-32-576': ' BUILTIN\\RDS Endpoint Servers', 'S-1-5-83-0': ' NT VIRTUAL MACHINE\\Virtual Machines', 'S-1-16-28672': ' Secure Process Mandatory Level ', 'S-1-5-11': ' Authenticated Users', 'S-1-1-0': ' Everyone', 'S-1-5-32-555': ' BUILTIN\\Remote Desktop Users', 'S-1-16-8448': ' Medium Plus Mandatory Level ', 'S-1-5-17': ' This Organization ', 'S-1-5-32-580': ' BUILTIN\\Remote Management Users', 'S-1-5-15': ' This Organization ', 'S-1-5-14': ' Remote Interactive Logon ', 'S-1-5-13': ' Terminal Server Users', 'S-1-5-12': ' Restricted Code', 'S-1-5-32-577': ' BUILTIN\\RDS Management Servers', 'S-1-5-10': ' Principal Self', 'S-1-3': ' Creator Authority', 'S-1-2': ' Local Authority', 'S-1-1': ' World Authority', 'S-1-0': ' Null Authority', 'S-1-5-32-574': ' BUILTIN\\Certificate Service DCOM Access ', 'S-1-5': ' NT Authority', 'S-1-4': ' Non-unique Authority', 'S-1-5-32-560': ' BUILTIN\\Windows Authorization Access Group', 'S-1-16-20480': ' Protected Process Mandatory Level ', 'S-1-5-64-10': ' NTLM Authentication ', 'S-1-5-32-558': ' BUILTIN\\Performance Monitor Users'}
+    if sid in known_sids:
+        return "%s (%s)" % (sid, known_sids.get(sid,sid))
+    return sid
+
 def BinarySIDtoStringSID(sid):
-  #Source: https://github.com/google/grr/blob/master/grr/parsers/wmi_parser.py
+  #Original form Source: https://github.com/google/grr/blob/master/grr/parsers/wmi_parser.py
   """Converts a binary SID to its string representation.
   https://msdn.microsoft.com/en-us/library/windows/desktop/aa379597.aspx
   The byte representation of an SID is as follows:
@@ -52,7 +60,8 @@ def BinarySIDtoStringSID(sid):
                          len(authority), authority)
       str_sid_components.append(struct.unpack("<L", authority)[0])
       start += 4
-  return "S-%s" % ("-".join([str(x) for x in str_sid_components]))
+      sid_str = "S-%s" % ("-".join([str(x) for x in str_sid_components]))
+      return lookup_sid(sid_str)
 
 def ole_timestamp(binblob):
     #converts a hex encoded OLE time stamp to a time string
@@ -151,9 +160,14 @@ def format_output(val,eachformat, eachstyle):
         val = file_timestamp(val)
         val = val.strftime(eachformat[5:])
     elif eachformat.lower() == "lookup_id":
-        val = id_table[val]
+        val = id_table.get(val, "No match in srum lookup table for %s" % (val))
     elif eachformat.lower() == "lookup_luid":
         val = lookup_luid(val)
+    elif eachformat.lower() == "lookup_sid":
+        val = "%s (%s)" % (val, lookup_sid(val))
+    elif eachformat.lower() == "seconds":
+        val = val/86400.0
+        new_cell.number_format = 'dd hh:mm:ss'
     elif eachformat.lower() == "md5":
         val = hashlib.md5(str(val)).hexdigest()
     elif eachformat.lower() == "sha1":
